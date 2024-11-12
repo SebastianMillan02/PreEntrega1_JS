5cc6149c3959ba0e943e429732c3d3b3824e244b
Sebastián Millán <contacto@sebastianmillan.com>
2024-11-12 15:11:35 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Pre Entrega 1 - JavaScript - Comisión 63330
+
+Esta entrega consta de un simulador interactivo sencillo de cotización de servicios de diseño y desarrollo web, diseñado con prompts y alerts para la interacción con el usuario. 
+
+En el flujo del programa el usuario podrá consultar los diferentes tipos de servicios ofrecidos, seleccionar cuál de todos esos servicios desea cotizar y responder preguntas relacionadas al servicio seleccionado para poder brindar un precio más personalizado y acercado a sus necesidades específicas. Luego de terminar con la cotización del servicio seleccionado podrá salir del programa o seleccionar un nuevo servicio a cotizar.
+
+## Conocimientos aplicados
+
+Para esta entrega se usaron diferentes técnicas aprendidas en el curso, como lo son: 
+
+- Uso de let y const para declaración e inicialización de variables. 
+- Ciclos while y do-while
+- Condicionales if-else, también if-else if
+- Condicional con Switch
+- ParseInt
+- Métodos para conversión de strings (.toLowerCase())
+
+## Autor
+
+- [Sebastián Millán Higuita](https://github.com/SebastianMillan02)
---
+++ frontend/js/main.js
@@ -0,0 +1,130 @@
+//Inicio de ciclo do-while para que se ejecute la cotización hasta que el usuario indique que quiere salir de la misma
+let entradaUsuario;
+
+do {
+  //Se define el servicio que desea cotizar el cliente
+  entradaUsuario = prompt(
+    "Ingresa el servicio que deseas cotizar o 'salir' para finalizar tu cotización. Para conocer los servicios disponibles ingresa la palabra 'servicios'"
+  );
+
+  let cantidadInternas;
+
+  //Switch para definir el servicio que ingresó el usuario
+  switch (entradaUsuario.toLowerCase()) {
+    case "sitio web":
+      const precioInterna = 250000;
+
+      cantidadInternas = parseInt(
+        prompt("Ingresa la cantidad de páginas internas que va a tener tu web")
+      );
+
+      //Ciclo para definir que el usuario está ingresando un tipo y una cantidad de internas válida y repetir el prompt en caso de que no sea así.
+      while (isNaN(cantidadInternas) || cantidadInternas <= 0) {
+        alert(
+          "Por favor ingresa un valor válido para la cantidad de páginas internas que va a tener tu sitio web"
+        );
+        cantidadInternas = parseInt(
+          prompt(
+            "Ingresa la cantidad de páginas internas que va a tener tu web"
+          )
+        );
+      }
+
+      //Cálculo del valor final estimado por la cantidad de internas especificada
+      const precioWeb = cantidadInternas * precioInterna;
+      alert(
+        `El valor estimado de tu sitio web con ${cantidadInternas} páginas internas es de ${precioWeb}`
+      );
+
+      break;
+
+    case "mantenimiento":
+      const mantenimientoMensual = 60000;
+      const mantenimientoAnual = 600000;
+
+      cantidadInternas = parseInt(
+        prompt("Ingresa la cantidad de páginas internas que tiene tu web")
+      );
+
+      //Ciclo para definir que el usuario está ingresando un tipo y una cantidad de internas válida
+      while (isNaN(cantidadInternas) || cantidadInternas <= 0) {
+        alert(
+          "Por favor ingresa un valor válido para la cantidad de páginas internas que tiene tu sitio web"
+        );
+        cantidadInternas = parseInt(
+          prompt("Ingresa la cantidad de páginas internas que tiene tu web")
+        );
+      }
+
+      //Definición del tipo de mantenimiento para entrar a un condicional con cambios de valor para cada uno de los mantenimientos
+      let tipoMantenimiento = prompt(
+        "Ingresa el tipo de mantenimiento que deseas para tu web: 'mensual' o 'anual'"
+      );
+
+      //Condicional para identificar el precio final del mantenimiento basado en la decisión de mantenimiento mensual o anual que elija el cliente
+      if (tipoMantenimiento == "mensual") {
+        alert(
+          `El mes de mantenimiento de tu web tiene un costo de ${
+            mantenimientoMensual * cantidadInternas
+          } pesos`
+        );
+      } else {
+        alert(
+          `El año de mantenimiento de tu web tiene un costo de ${
+            mantenimientoAnual * cantidadInternas
+          } pesos`
+        );
+      }
+      break;
+
+    case "clases":
+      let valorHora = 40000;
+
+      let horasClase = parseInt(
+        prompt("Ingresa la cantidad de horas de clase que necesitas ver")
+      );
+
+      //Ciclo para asegurar que el usuario está ingresando un tipo y una cantidad de horas válida
+      while (isNaN(horasClase) || horasClase <= 0) {
+        alert(
+          "Por favor ingresa un valor válido para la cantidad de horas de clase que deseas cotizar"
+        );
+
+        horasClase = parseInt(
+          prompt("Ingresa la cantidad de horas de clase que necesitas ver")
+        );
+      }
+
+      //Cálculo del valor de las clases según la cantidad de horas especificadas por el usuario
+      let valorClases = horasClase * valorHora;
+
+      //Calcular descuento de clases si el cliente contrata 5 o más horas
+      if (horasClase >= 5) {
+        valorHora = 35000;
+
+        alert(
+          `Por contratar 5 o más horas, tienes un descuento de 5000 en cada hora de clase. El valor total de tus clases sería de ${valorClases}`
+        );
+      } else {
+        alert(`El valor total de tus clases sería de ${valorClases}`);
+      }
+      break;
+
+    //Caso para que el usuario vea la lista de servicios disponibles
+    case "servicios":
+      alert(
+        "En los servicios disponibles tenemos 'sitio web' - 'mantenimiento' - 'clases'"
+      );
+      break;
+
+    //Finalización de la cotización
+    case "salir":
+      alert("Gracias por tu cotización.");
+      break;
+
+    //Alternativa para que el usuario sepa que no hay una opción con los términos que ingresó
+    default:
+      alert("Selecciona un servicio válido");
+      break;
+  }
+} while (entradaUsuario.toLowerCase() !== "salir");
